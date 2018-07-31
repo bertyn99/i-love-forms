9ffafc7a309d9454325179d1ee3e2b2d2ee34a6a
bertyn99 <bboulikou@gmail.com>
2018-07-31 13:54:58 +0200

    ajout d affichage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
     def new
-        @users=params
+        @users=Users.new
     end
 
     def create
-        
-
+        @users=params[:user_username]
+        @users=params[:user_email]
+        @users=params[:user_bio]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#new', as:'user'
   post '/users', to: 'users#create'
-
+  get '/account', to:'user#create'
 end
